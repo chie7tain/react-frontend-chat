adbf428413255a23aca9d6e14c719c719b9252d6
chei7tain <fredrickokwuobi@gmail.com>
2022-02-09 23:58:50 +0100

    add chat component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Chat from "./components/Chat/Chat";
 // components
 import { Sidebar } from "./components/Sidebar/Sidebar";
 function App() {
   return (
     <div className="App">
       <Sidebar />
+      <Chat />
     </div>
   );
 }
