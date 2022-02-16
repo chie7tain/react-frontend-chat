ac525ee137e4c8c69cc79f45c3199703fbf5c628
chei7tain <fredrickokwuobi@gmail.com>
2022-02-16 22:33:59 +0100

    implement the login feature using axios

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -9,45 +9,13 @@ const Input = () => {
   const [loading, setLoading] = useState(false);
   const [chat, setChats] = useState([]);
   const [value, setValue] = useState("");
-  // "https://jsonplaceholder.typicode.com/posts";
+
+
   useEffect(() => {
     const fetchData = async () => {
       try {
         setLoading(true);
-        // var data = JSON.stringify({
-        //   email: "fredrickokwuobi@gmail.com",
-        //   password: "123456789"
-        // });
 
-        // const config: AxiosRequest= {
-        //   method: "post",
-        //   url: "http://127.0.0.1:3050/api/v1/user/login",
-        //   headers: {
-        //     "Content-Type": "application/json",
-        //     Cookie:
-        //       "connect.sid=s%3AtUvdI_13BB1JQ3fhR8YnyevIAk-_kp5t.4WBbQ55U1a0MAvUq6lu0KjtQ3kQDeVOYYeCcHvEcRCg",
-        //   },
-        //   data: data,
-        // };
-        // users / user / friends / favorite;
-        // let res = await axios({
-        //   method: "get",
-        //   url: "http://127.0.0.1:3050/api/v1/users/user/friends/favorite;",
-        //   headers: {
-        //     "Content-Type": "application/json",
-        //     Authorization:
-        //       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZjk3ZDc4OGE5OGRmMzhiYzk5ZGQ3MiIsImlhdCI6MTY0NDcwNDcwN30.DDTr5GO4iK6g-by-V5GkneCOaOYRrPL4yE2NucrC1AI",
-        //   },
-        //   data: data,
-        // });
-        // .then(function (response) {
-        //   console.log(JSON.stringify(response.data));
-        // })
-        // .catch(function (error) {
-        //   console.log(error);
-        // });
-
-        // console.log(JSON.stringify(response.data));
         const axios = require("axios");
         let data = JSON.stringify({
           email: "fredrickokwuobi@gmail.com",
@@ -64,29 +32,24 @@ const Input = () => {
           },
           data: data,
         };
-
-        let response = axios(config);
-
-        // .then((response: { data: any }) => {
-        //   console.log(JSON.stringify(response.data));
-        //   console.log(response.data);
-        // })
-        // .catch((error: any) => {
-        //   console.log(error);
-        // });
-
-        let res = await axios({
+        let friendsUrl =  'http://127.0.0.1:3050/api/v1/users/user/friends';
+        let groupsUrl = ""
+        let response = await axios(config);
+        let { accessToken } = response.data;
+        console.log(accessToken);
+        let favConfig = {
           method: "get",
-          url: "http://127.0.0.1:3050/api/v1/users/user/friends/favorite;",
+          url: "http://127.0.0.1:3050/api/v1/users/user/friends/favorite",
           headers: {
-            "Content-Type": "application/json",
-            Authorization:
-              "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZjk3ZDc4OGE5OGRmMzhiYzk5ZGQ3MiIsImlhdCI6MTY0NDcwNDcwN30.DDTr5GO4iK6g-by-V5GkneCOaOYRrPL4yE2NucrC1AI",
+            Authorization: `Bearer ${accessToken}`,
+            Cookie:
+              "connect.sid=s%3AM3oAXCWrsMzHbcvZjNCebbZqsE2icTvX.TlZUBHIvRujgMAvQ225ahFLZ%2F5262KJj1nOtzEcM8ZE",
           },
-          data: data,
-        });
+        };
 
-        // setChats(response.data);
+        let favRes = await axios(favConfig);
+        console.log(favRes.data);
+        setChats(favRes.data);
         setLoading(false);
       } catch (err: any) {
         console.log(err.response.data);
